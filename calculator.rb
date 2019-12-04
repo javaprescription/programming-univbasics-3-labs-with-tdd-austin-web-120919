78ac593154e01017d06f8600e083ab486b0c11d6
Austin Scott <austinscott4@gmail.com>
2019-12-04 00:57:33 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,9 @@
-# Add your variables here
+first_number = 4
+second_number = 8
+
+def calculator(first_number, second_number)
+  sum = first_number+second_number
+  difference = first_number-second_number
+  product = first_number*second_number
+  quotient = first_number/second_number
+end
